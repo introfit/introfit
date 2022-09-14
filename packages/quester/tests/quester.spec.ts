a337db7cb7e54ed38cdac0d3843bd80b01b9e516
YiJie <yijie4188@gmail.com>
2022-09-14 10:49:49 +0800

    feat(quester): unit test demo

--- packages/quester/tests/quester.spec.ts
+++ packages/quester/tests/quester.spec.ts
@@ -5,10 +5,32 @@ import { AxiosAdapter } from '../src/axios-adapter'
 
 use(cap)
 
+type DefProp<N extends string, P> = { [K in N]: Promise<P> }
+
 describe('Quester', function () {
+  interface User {
+    id: number
+    name: string
+  }
+  interface Guild {
+    id: number
+    name: string
+  }
+  interface Api {
+    user(id: string):
+      & Promise<User>
+      & DefProp<'friend', User>
+      & DefProp<'friends', User[]>
+      & DefProp<'guilds', Guild[]>
+    users: Promise<User[]>
+  }
   class Api extends Quester<AxiosAdapter> {
   }
-  it('should travel simple property and simple method.', () => {
+  const api = new Api(AxiosAdapter, {});
+  it('should travel simple property and simple method.', async () => {
+    const users = await api.users
+    const user001 = await api.user('001')
+    const user001Friends = await api.user('001').friends
   })
   it('should travel nest property and nest method.', () => {
   })
